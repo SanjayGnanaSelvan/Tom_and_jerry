8ce466f192782ec8bcd2e11be1cbb0f612ffde92
SanjayGnanaSelvan <78967023+SanjayGnanaSelvan@users.noreply.github.com>
2021-05-02 08:53:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,13 +11,13 @@ var gardenImg
 
     function preload(){
         gardenImg = loadImage("images/garden.png");
-        tomImg1 = loadImage("images/cat1.png");
+        tomImg1 = loadAnimation("images/cat1.png");
         tomImg2= loadAnimation("images/cat2.png", "images/cat3.png");
-        tomImg3 = loadImage("images/cat4.png");
+        tomImg3 = loadAnimation("images/cat4.png");
         
-        jerryImg1 = loadImage("images/mouse1.png")
+        jerryImg1 = loadAnimation("images/mouse4.png");
         jerryImg2 = loadAnimation("images/mouse2.png","images/mouse3.png");
-        jerryImg3 = loadAnimation("images/mouse4.png");
+        jerryImg3 = loadAnimation("images/mouse1.png");
         
         
       }
@@ -35,7 +35,7 @@ function setup(){
     tom.scale=0.2;
 
     jerry = createSprite(150,600);
-    jerry.addAnimation("jerry", jerryImg3)
+    jerry.addAnimation("jerry", jerryImg1)
     jerry.scale = 0.1;
 
 }
@@ -51,30 +51,44 @@ function draw() {
     //}
 
     if(tom.x - jerry.x < (tom.width - jerry.width)/2){
-        tom.changeAnimation("tomCollided", tomImg3)
+        tom.addAnimation("tom_collided", tomImg3)
+        tom.changeAnimation("tom_collided");
         tom.velocityX = 0;
-        jerry.changeAnimation(jerryImg1);
-    }
+        jerry.addAnimation("jerry_collided", jerryImg3)
+        jerry.changeAnimation("jerry_collided");    }
+
+
+    
+
+      
+
 
     drawSprites();
     textSize(20);
     fill(255);
 }
 
-
 function keyPressed(){
 
     if(keyCode === LEFT_ARROW) { 
-        tom.velocityX = -1;
+        tom.velocityX = -5;
         tom.addAnimation("tom_running", tomImg2)
         tom.changeAnimation("tom_running");
+        
         
       }
 
       if(keyCode === RIGHT_ARROW) { 
         jerry.addAnimation("jerry_teasing", jerryImg2)
         jerry.changeAnimation("jerry_teasing");
-        jerry.frameDelay = 25;
+        jerry.frameDelay = 10;
+
+        tom.addAnimation("tom_sitting", tomImg1)
+        tom.changeAnimation("tom_sitting");
       }
 
 }
+
+
+
+   
